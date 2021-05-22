b0d041d129adb9edb151ac612fbff9d11d4e851d
Hetvi Shah <68198839+hetvshah@users.noreply.github.com>
2021-05-22 00:56:10 -0500

    todo task on home page on frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,35 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const tasks = [
+    {
+      todo: 'Finish part 7 of CIS 121 programming. ',
+      notes: 'Make sure to pay attention to runtimes.',
+    },
+    {
+      todo: 'Respond to emails.',
+      notes: 'Specifically John Doe and research mentor.',
+    },
+    {
+      todo: 'Search for apartments.',
+      notes: '',
+    },
+  ];
+
+  const listTasks = tasks.map((task) => (
+    <div className="todo">
+      <h3>
+        <input type="checkbox" />
+        {task.todo}
+      </h3>
+      <p>{task.notes}</p>
+    </div>
+  ));
+
   return (
     <div>
       <div className="header">
-        <p style={{ marginLeft: '3.5vw' }}>Hi Hetvi!</p>
+        <p style={{ marginLeft: '7.5vw' }}>Hi Hetvi!</p>
         <div className="topLinks">
           <p className="links">
             <Link to="/planning">
@@ -20,9 +45,13 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <p className="header" style={{ marginLeft: '3.5vw', fontSize: '1.5vw' , marginTop: '2.5vw'}}>
+      <p
+        className="header"
+        style={{ marginLeft: '7.5vw', fontSize: '1.25vw', marginTop: '2.5vw' }}
+      >
         Today is Friday, May 21st. Here is your to-do list for today, good luck!
       </p>
+      {listTasks}
     </div>
   );
 };
