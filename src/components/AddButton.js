6d6c959e892a1591fa1791ec82d6f1d7b647699a
Hetvi Shah <68198839+hetvshah@users.noreply.github.com>
2021-06-07 15:10:13 -0500

    minus icon

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -2,10 +2,10 @@ import { GrAdd } from 'react-icons/gr';
 import { HiMinus } from 'react-icons/hi';
 import './Styles.css';
 
-const Button = ({ onClick, showAdd }) => {
+const Button = ({ onClick, showAddTask }) => {
   return (
     <button onClick={onClick} className="home-btn">
-      {showAdd ? (
+      {showAddTask ? (
         <HiMinus className="hi-minus" />
       ) : (
         <GrAdd className="gr-add" />
